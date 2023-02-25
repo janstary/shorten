550d8cc8bcd6c6d25c31cfb32b6d3cf519393df7
Jan Starý <hans@stare.cz>
2023-02-25 16:20:08 +0100

    whitespace
    This merely runs shorten.c and dupfinfo.c through indent(1)
    for easier editing later.

--- dupfinfo.c
+++ dupfinfo.c
@@ -6,7 +6,7 @@
 *                                                                             *
 ******************************************************************************/
 
-/* 
+/*
   set the atime and mtime of path1 to be the same as that of path0
 */
 
@@ -14,37 +14,43 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #if defined(MSDOS) || defined(_WINDOWS)
-# include <io.h>
-# include <sys/utime.h>
+#include <io.h>
+#include <sys/utime.h>
 #else
-# include <utime.h>
-# include <unistd.h>
+#include <utime.h>
+#include <unistd.h>
 #endif
 #include "shorten.h"
 
-int dupfileinfo(path0, path1) char *path0, *path1; {
-  int errcode;
-  struct stat buf;
+int
+dupfileinfo(path0, path1)
+	char *path0, *path1;
+{
+	int errcode;
+	struct stat buf;
 
-  errcode = stat(path0, &buf);
-  if(!errcode) {
-    struct utimbuf ftime;
+	errcode = stat(path0, &buf);
+	if (!errcode) {
+		struct utimbuf ftime;
 
-    /* do what can be done, and igore errors */
-    (void) chmod(path1, buf.st_mode);
-    ftime.actime  = buf.st_atime;
-    ftime.modtime = buf.st_mtime;
-    (void) utime(path1, &ftime);
+		/* do what can be done, and igore errors */
+		(void)chmod(path1, buf.st_mode);
+		ftime.actime = buf.st_atime;
+		ftime.modtime = buf.st_mtime;
+		(void)utime(path1, &ftime);
 #ifdef unix
-    (void) chown(path1, buf.st_uid, -1);
-    (void) chown(path1, -1, buf.st_gid);
+		(void)chown(path1, buf.st_uid, -1);
+		(void)chown(path1, -1, buf.st_gid);
 #endif
-  }
-  return(errcode);
+	}
+	return (errcode);
 }
 
 #ifdef PROGTEST
-int main(int argc, char **argv) {
-  return(dupfileinfo(argv[1], argv[2]));
+int
+main(int argc, char **argv)
+{
+	return (dupfileinfo(argv[1], argv[2]));
 }
+
 #endif
